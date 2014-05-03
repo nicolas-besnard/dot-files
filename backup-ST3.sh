76cd7c4286e13a61c391d839e8c3898c0ef3ef5e
Nicolas Besnard <besnard.nicolas@gmail.com>
2014-05-04 00:32:40 +0200

    Save ST3 config

---
+++ backup-ST3.sh
@@ -0,0 +1,2 @@
+cp -rf ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/* ST3
+rm ST3/Package\ Control.last-run
---
+++ restore-ST3.sh
@@ -0,0 +1,1 @@
+cp -rf ST3/* ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
