9e6e25e7f606f01217cff8a6ed427f5447aa18c1
Nicolas Besnard <besnard.nicolas@gmail.com>
2014-07-25 19:39:18 +0200

    Add DDB

---
+++ dynamodb.sh
@@ -0,0 +1,6 @@
+wget https://s3-us-west-2.amazonaws.com/dynamodb-local/dynamodb_local_2013-12-12.tar.gz
+
+tar -xvf dynamodb_local_2013-12-12.tar.gz
+rm dynamodb_local_2013-12-12.tar.gz
+
+mv dynamodb_local_2013-12-12 /Applications/dynamodb
