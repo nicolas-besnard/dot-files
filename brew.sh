7f631cae2988a6de9016ab871ccf25722a3c67c1
Nicolas Besnard <besnard.nicolas@gmail.com>
2014-08-12 23:28:28 +0200

    - Default app when installing brew - Change default version of ES to preserve old script language

--- brew.sh
+++ brew.sh
@@ -1,1 +1,8 @@
 ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+brew doctor
+brew install caskroom/cask/brew-cask
+
+brew cask install google-chrome
+brew cask install skype
+brew cask install iterm2
+brew cask install vlc
--- elasticsearch.sh
+++ elasticsearch.sh
@@ -1,5 +1,6 @@
-wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.3.0.zip
-unzip elasticsearch-1.3.0.zip
-rm elasticsearch-1.3.0.zip
-elasticsearch-1.3.0/bin/plugin -install mobz/elasticsearch-head
-mv elasticsearch-1.3.0 /Applications/elasticsearch
+#wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.3.0.zip
+wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.1.2.zip
+unzip elasticsearch-1.1.2.zip
+rm elasticsearch-1.1.2.zip
+elasticsearch-1.1.2/bin/plugin -install mobz/elasticsearch-head
+mv elasticsearch-1.1.2 /Applications/elasticsearch
