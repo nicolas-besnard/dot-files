91bbb91e99ebc671b5ac64369bf42459ce61e3cc
Nicolas Besnard <besnard.nicolas@gmail.com>
2015-04-02 00:08:00 +0100

    Update aws.sh

--- aws.sh
+++ aws.sh
@@ -1,7 +1,3 @@
 pip install awscli
-pit install boto
-wget https://s3.amazonaws.com/elasticbeanstalk/cli/AWS-ElasticBeanstalk-CLI-2.6.3.zip
-unzip AWS-ElasticBeanstalk-CLI-2.6.3.zip
-rm WS-ElasticBeanstalk-CLI-2.6.3.zip
-mv AWS-ElasticBeanstalk-CLI-2.6.3/ /Applications/AWS_beanstalk/
-echo "PATH=$PATH:/Applications/AWS_beanstalk/eb/macosx/python2.7" >> .zshrc
+pip install boto
+sudo pip install awsebcli
