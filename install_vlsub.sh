61740c515e6358a267b12a6983b59953966ed243
Nicolas Besnard <besnard.nicolas@gmail.com>
2017-02-08 14:19:19 +0100

    VLCSUB

--- install_vlsub.sh
+++ install_vlsub.sh
@@ -2,8 +2,8 @@
 
 file_path=/Applications/VLC.app/Contents/MacOS/share/lua/extensions/vlsub.lua
 
-if [ -f file_path ]; then
-    wget https://raw.githubusercontent.com/exebetche/vlsub/master/vlsub.lua -O file_path
-else
+if [ -f $file_path ]; then
     echo "VLSub already installed"
+else
+    wget https://raw.githubusercontent.com/exebetche/vlsub/master/vlsub.lua -O $file_path
 fi
