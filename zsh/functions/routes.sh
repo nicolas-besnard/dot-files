0a9f5fc8d408b38939a952ae2a509e49e3a248be
Nicolas Besnard <besnard.nicolas@gmail.com>
2019-03-18 15:44:43 +0100

    Update routes.sh

--- zsh/functions/routes.sh
+++ zsh/functions/routes.sh
@@ -1,16 +1,28 @@
 routes() {
+  CACHE_FILE='tmp/routes'
+
   # regenerate tmp/routes if it is not already generated or it is older than the routes config file
-  if [[ ! -f tmp/routes ]] || [[ tmp/routes -ot config/routes.rb ]]; then
-    rake routes >! tmp/routes
+  if [[ ! -f $CACHE_FILE ]] || [[ CACHE_FILE -ot config/routes.rb ]]; then
+    ROUTES="$(bundle exec rake routes)"
+
+    if [[ $? -ne 0 ]]; then
+      return 1
+    fi
+
+    echo "$ROUTES" > $CACHE_FILE
   fi
 
   if (($#)); then
     if type ag >/dev/null; then
-      ag --color --nonumbers "${(j:|:)@}" tmp/routes
+      ag --color --nonumbers "${(j:|:)@}" $CACHE_FILE
     else
-      grep "${(j:|:)@}" tmp/routes
+      grep "${(j:|:)@}" $CACHE_FILE
     fi
   else
-    cat tmp/routes
+    if type fzf >/dev/null; then
+      fzf $CACHE_FILE
+    else
+      cat $CACHE_FILE
+    fi
   fi
 }
