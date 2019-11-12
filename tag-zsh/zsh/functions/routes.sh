ccf1e64aa31a46ccb55a09f71a49437c20b9c647
Nicolas Besnard <besnard.nicolas@gmail.com>
2019-11-12 17:58:42 +0100

    improved routes

--- tag-zsh/zsh/functions/routes.sh
+++ tag-zsh/zsh/functions/routes.sh
@@ -1,8 +1,24 @@
+any_route_files_changed() {
+  if [ ! -d "config/routes" ]; then
+    return 1
+  fi
+
+  for route_file in config/routes/*.rb; do
+    if [ $1 -ot $route_file ]; then
+      echo "$route_file changed"
+      return 0
+    fi
+  done
+
+  return 1
+}
+
 routes() {
   CACHE_FILE=tmp/routes
 
-  # regenerate tmp/routes if it is not already generated or it is older than the routes config file
-  if [ $CACHE_FILE -ot config/routes.rb ] || [ ! -e $CACHE_FILE ]; then
+  any_route_files_changed $CACHE_FILE
+
+  if [ $? -eq 0 ] || [ $CACHE_FILE -ot config/routes.rb ] || [ ! -e $CACHE_FILE ]; then
     echo 'generating new cache...'
     ROUTES="$(bundle exec rake routes)"
 
@@ -15,13 +31,13 @@ routes() {
 
   if (($#)); then
     if type ag >/dev/null; then
-      ag --color --nonumbers "${(j:|:)@}" $CACHE_FILE
+      ag --color --nonumbers "${(j:|:)@}" $CACHE_FILE | xargs echo -n
     else
       grep "${(j:|:)@}" $CACHE_FILE
     fi
   else
     if type fzf >/dev/null; then
-      cat $CACHE_FILE | fzf
+      cat $CACHE_FILE | fzf | xargs echo -n
     else
       cat $CACHE_FILE
     fi
